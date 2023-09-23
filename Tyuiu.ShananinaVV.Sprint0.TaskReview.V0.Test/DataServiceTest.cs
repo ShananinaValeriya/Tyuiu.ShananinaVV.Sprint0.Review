5367af677da50ffebc46a861c9229204a3c1c33a
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-09-23 12:56:49 +0500

    Add project files.

---
+++ Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            int a = 10;
+            int b = 5;
+            int c = 12;
+
+            int res = DataService.Calc(a, b, c);
+
+            Assert.AreEqual(135, res);
+        }
+    }
+}
---
+++ Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShananinaVV.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78af7f12-e5a0-4de7-bebc-6db72bd6a1a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
